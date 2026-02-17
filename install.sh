93f2df0adc70dac1705bd267033b36c175abd6db
lior <lior.group100@gmail.com>
2026-02-17 20:56:53 +0200

    feat: update Omarchy installation path to user's home directory

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -eEo pipefail
 
 # Define Omarchy locations
-export OMARCHY_PATH="$HOME/.local/share/omarchy"
+export OMARCHY_PATH="$HOME/omarchy"
 export OMARCHY_INSTALL="$OMARCHY_PATH/install"
 export OMARCHY_INSTALL_LOG_FILE="/var/log/omarchy-install.log"
 export PATH="$OMARCHY_PATH/bin:$PATH"
